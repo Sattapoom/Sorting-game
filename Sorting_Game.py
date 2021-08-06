ccb4c0d5877823abc3f83e6a6010a9fe42630d75
Poom <s6301012630192@email.kmutnb.ac.th>
2021-08-06 12:34:05 +0700

    Poom_1

---
+++ Sorting_Game.py
@@ -0,0 +1,19 @@
+class Sorting_game():
+    def __init__(self):
+        self.board= [["A","B","C","D"], ["E","F","G","H"], ["I","J","K"," "]]
+        self.index_space = [0]*2
+    
+    def random_board_start(self):
+        pass
+
+    def display_board(self):
+        pass
+    
+    def check_endgame(self):
+        pass
+    
+    def move_character(self,c):
+        pass
+
+class input_processor():
+    pass
