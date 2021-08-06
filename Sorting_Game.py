eab6d04903d5378ffebd8f086ebc1dd6e91f4891
ThanabodeeSaepui <s6301012620138@email.kmutnb.ac.th>
2021-08-06 14:29:37 +0700

    Safe_1

--- Sorting_Game.py
+++ Sorting_Game.py
@@ -25,13 +25,44 @@ class Sorting_game():
     def display_board(self):
         for i in self.board:
             print(f"{i[0]} {i[1]} {i[2]} {i[3]}")
+        print("--------")
             
     def check_endgame(self):
         pass
     
-    def move_character(self,c):
-        pass
-
+    def move_character(self,c:str):
+        # top check
+        top_index = self.index_space[0] - 1
+        if top_index >= 0:
+            if self.board[top_index][self.index_space[1]] == c:
+                self.board[top_index][self.index_space[1]] = " "
+                self.board[self.index_space[0]][self.index_space[1]] = c
+                self.index_space[0] = top_index
+                return
+        # bottom check
+        bottom_index = self.index_space[0] + 1
+        if bottom_index <= 2:
+            if self.board[bottom_index][self.index_space[1]] == c:
+                self.board[bottom_index][self.index_space[1]] = " "
+                self.board[self.index_space[0]][self.index_space[1]] = c
+                self.index_space[0] = bottom_index
+                return
+        # left check
+        left_index = self.index_space[1] - 1
+        if left_index >= 0:
+            if self.board[self.index_space[0]][left_index] == c:
+                self.board[self.index_space[0]][left_index] = " "
+                self.board[self.index_space[0]][self.index_space[1]] = c
+                self.index_space[1] = left_index
+                return
+        # right check
+        right_index = self.index_space[1] + 1
+        if right_index <= 3:
+            if self.board[self.index_space[0]][right_index] == c:
+                self.board[self.index_space[0]][right_index] = " "
+                self.board[self.index_space[0]][self.index_space[1]] = c
+                self.index_space[1] = right_index
+                return
 class input_processor():
     def __init__(self):
         self.letter = ""
@@ -49,3 +80,11 @@ class input_processor():
         return self.letter
 #game_sort = input_processor()
 #print(game_sort.get_input())
+if __name__ == '__main__':
+    game = Sorting_game()
+    # game.random_board_start()
+    game.display_board()
+    while True:
+        c = input("INPUT : ")
+        game.move_character(c)
+        game.display_board()
